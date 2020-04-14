2a12955554d26b37076f43fd9b3cd03d6d214e60
aneelia <aneelia@bk.ru>
2020-04-14 23:22:24 +0300

    feat(html): add html attributes for tabs

--- js/custom.js
+++ js/custom.js
@@ -7,17 +7,74 @@
   });
 })();
 
-document.querySelectorAll("#nav li").forEach(function(navEl) {
-  navEl.onclick = function() {
-    toggleTab(this.id, this.dataset.target);
+var navEls = document.querySelectorAll("#nav li");
+
+navEls.forEach(function(elem, index) {
+  elem.onclick = () => {
+    toggleTab(elem.id, elem.dataset.target);
   };
+
+  elem.addEventListener("keydown", e => {
+    const firstTab = navEls[0];
+    const lastTab = navEls[navEls.length - 1];
+
+    switch (e.code) {
+      case "ArrowRight": {
+        const nextTab = navEls[index + 1];
+
+        if (nextTab) {
+          nextTab.focus();
+        } else {
+          firstTab.focus();
+        }
+
+        break;
+      }
+
+      case "ArrowLeft": {
+        const prevTab = navEls[index - 1];
+
+        if (prevTab) {
+          prevTab.focus();
+        } else {
+          lastTab.focus();
+        }
+
+        break;
+      }
+
+      case "Enter":
+      case "Space": {
+        elem.click();
+
+        break;
+      }
+
+      case "Home": {
+        e.preventDefault();
+        firstTab.focus();
+        break;
+      }
+
+      case "End": {
+        e.preventDefault();
+        lastTab.focus();
+        break;
+      }
+
+      default:
+        break;
+    }
+  });
 });
 
 function toggleTab(selectedNav, targetId) {
-  var navEls = document.querySelectorAll("#nav li");
-
   navEls.forEach(function(navEl) {
-    if (navEl.id == selectedNav) {
+    const isActive = navEl.id == selectedNav;
+    navEl.setAttribute("aria-selected", isActive ? "true" : "false");
+    navEl.setAttribute("tab-index", isActive ? "0" : "-1");
+
+    if (isActive) {
       navEl.classList.add("is-active");
     } else {
       if (navEl.classList.contains("is-active")) {
